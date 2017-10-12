a481fd0171f7f951e67e2cab445ca5b4c6cdc3bb
Raul Perez <raulperod@gmail.com>
2017-10-11 22:58:02 -0700

    tarea1

--- Tarea1/productoPunto.cu
+++ Tarea1/productoPunto.cu
@@ -1,48 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 512
+#define N (2048*2048)
+#define THREADS_PER_BLOCK 512
 
-__global__ void dot( int *a, int *b, int *c ){
-
-    __shared__ int temp[N];
-    temp[threadIdx.x] = a[threadIdx.x] * b[threadIdx.x];
-    __syncthreads(); 
-    if( 0 == threadIdx.x ){
+__global__ void dot( int *a, int *b, int *c ) {
+    __shared__ int temp[THREADS_PER_BLOCK];
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    temp[threadIdx.x] = a[index] * b[index];
+    __syncthreads();
+    if( 0 == threadIdx.x ) {
         int sum = 0;
-        for( int i = 0; i < N; i++ ){
+        for( int i = 0; i < THREADS_PER_BLOCK; i++ ){
             sum += temp[i];
         }
-        *c = sum;
+        atomicAdd( c , sum );
     }
 }
 
 void llenarVector(int*);
 
-int main(){
-    int *a, *b, *c; 
-    int *dev_a, *dev_b, *dev_c; 
-    int size = N * sizeof( int ); 
-    
+int main( void ) {
+    int *a, *b, *c; // host copies of a, b, c
+    int *dev_a, *dev_b, *dev_c; // device copies of a, b, c
+    int size = N * sizeof( int ); // we need space for N ints
+    // allocate device copies of a, b, c
     cudaMalloc( (void**)&dev_a, size );
     cudaMalloc( (void**)&dev_b, size );
     cudaMalloc( (void**)&dev_c, sizeof( int ) );
-
-    a = (int*)malloc( size );
-    b = (int*)malloc( size );
-    c = (int*)malloc( sizeof( int ) );
+    a = (int *)malloc( size );
+    b = (int *)malloc( size );
+    c = (int *)malloc( sizeof( int ) );
     // inicializo los vectores a y b
     llenarVector( a );
     llenarVector( b );
     // mandas a y b al GPU
     cudaMemcpy( dev_a, a, size, cudaMemcpyHostToDevice );
     cudaMemcpy( dev_b, b, size, cudaMemcpyHostToDevice );
-    // realizo el calculo
-    dot<<< 1, N >>>( dev_a, dev_b, dev_c );
-    // obtengo el valor del producto de la GPU
+    // realizas el calculo
+    dot<<< (N+1)/THREADS_PER_BLOCK, THREADS_PER_BLOCK >>>( dev_a, dev_b, dev_c );
+    // obtienes el producto punto
     cudaMemcpy( c, dev_c, sizeof( int ) , cudaMemcpyDeviceToHost );
-
-    printf("El producto punto es: %d\n", *c);
-    // libera la memoria
+    printf("El producto punto es: %d", *c)
     free( a ); 
     free( b ); 
     free( c );
@@ -53,7 +51,5 @@ int main(){
 }
 
 void llenarVector(int *a){
-    int *v;
-    v = a;
-    for(int i=0 ; i < N ; i++){ v[i] = 1; }
+    for(int i=0 ; i < N ; i++){ a[i] = 1; }
 }
---
+++ Tarea1/sumaMatrices.cu
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 2048
+#define THREADS_PER_BLOCK 512
+
+__global__ void suma( int *a, int *b, int *c ) {
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    c[index] = a[index] + b[index]
+}
+
+void llenarMatriz(int*);
+
+int main( void ) {
+    int *a, *b, *c;
+    int *dev_a, *dev_b, *dev_c; 
+    int size = N * N * sizeof( int ); 
+    
+    cudaMalloc( (void**)&dev_a, size );
+    cudaMalloc( (void**)&dev_b, size );
+    cudaMalloc( (void**)&dev_c, size );
+    a = (int*)malloc( size );
+    b = (int*)malloc( size );
+    c = (int*)malloc( size );
+    
+    llenarMatriz( a );
+    llenarMatriz( b );
+    
+    cudaMemcpy( dev_a, a, size, cudaMemcpyHostToDevice );
+    cudaMemcpy( dev_b, b, size, cudaMemcpyHostToDevice );
+    
+    dot<<< (N*N+1)/THREADS_PER_BLOCK, THREADS_PER_BLOCK >>>( dev_a, dev_b, dev_c );
+    
+    cudaMemcpy( c, dev_c, size , cudaMemcpyDeviceToHost );
+    
+    free( a ); 
+    free( b ); 
+    free( c );
+    cudaFree( dev_a );
+    cudaFree( dev_b );
+    cudaFree( dev_c );
+    return 0;
+}
+
+void llenarMatriz(int *m){
+    for(int i=0 ; i < N ; i++){ 
+        for(int j=0 ; j < N ; j++){
+            m[j+i*N] = 1; 
+        }
+    }
+}
